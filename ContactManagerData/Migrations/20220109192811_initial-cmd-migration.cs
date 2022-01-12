94eb939ad5a0b50ca8a57cf19153737c38690f56
Ron Hansen <rhansen55@inbox.com>
2022-01-12 15:31:25 -0500

    Updated project to reference new data layer

---
+++ ContactManagerData/Migrations/20220109192811_initial-cmd-migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManagerData.Migrations
+{
+    public partial class initialcmdmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
